332074e3b5a01d24a5ad532b03a04e858abdc320
Darpan Patel <darpanpatel3131@gmail.com>
2020-07-10 15:46:10 -0300

    report UI tabs added

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,11 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Tabs, Tab } from "react-bootstrap";
 
-export default class Reports extends Component {
-  render() {
-    return (
-      <div>
-        <h6>Report Type:</h6>
-      </div>
-    );
-  }
+export default function Reports() {
+  const [key, setKey] = useState("by quiz");
+  return (
+    <div>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="by quiz" title="By Quizzes">
+          <p>this is a report by quiz tab</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Tab>
+        <Tab eventKey="By User" title="By Users">
+          <p>this is report by User tab</p>
+        </Tab>
+        <Tab eventKey="By Classroom" title="By Classrooms">
+          <p>this is report by classroom tab</p>
+        </Tab>
+      </Tabs>
+    </div>
+  );
 }
