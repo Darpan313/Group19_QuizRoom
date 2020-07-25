983242f75875f4a997fd9ea61f0156f7173b9b28
Krutarth Patel <kr739904@dal.ca>
2020-07-25 12:38:28 -0300

    Added function that send email using emaijs

--- src/components/Support.js
+++ src/components/Support.js
@@ -11,6 +11,16 @@ const rows = [
     createData("Quiz 1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
   ];
 
+  
+  function sendEmail(e) {
+    e.preventDefault();    
+
+    emailjs.sendForm('gmail', 'template_PjL3dMN0', e.target, 'user_WCE3K6J0MJmpEMk0m5IYq')
+      .then((result) => {
+      }, (error) => {
+      });
+  }
+
 export default class Support extends Component {
     
     render() {
