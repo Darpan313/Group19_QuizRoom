71f7d90b89784162827e8864b96f47ba7f60b4d7
Krutarth Patel <kr739904@dal.ca>
2020-07-25 14:27:17 -0300

    Updated FAQs to use data from mongoDB collection

--- src/components/Support.js
+++ src/components/Support.js
@@ -6,12 +6,6 @@ function createData(question, answer) {
     return { question, answer };
 }
 
-const rows = [
-    createData("Quiz 1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."),
-    createData("Quiz 1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."),
-    createData("Quiz 1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
-];
-
 function sendEmail(e) {
     e.preventDefault();
 
@@ -41,10 +35,11 @@ export default class Support extends Component {
                 let questionList = []
                 for (i; i < data.length; i++) {
                     questionList.push(createData(data[i]["question"], data[i]["answer"]))
+                    this.setState({ rows: questionList })
                 }
             });
     }
-
+    
     render() {
         return (
             <div class="container-fluid">
