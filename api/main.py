b73793de0013af10dc7fd19138bc59a82db68142
ddpatel <dp827448@dal.ca>
2020-07-25 22:49:55 -0300

    Added Backend for Class management

--- api/main.py
+++ api/main.py
@@ -1,19 +1,24 @@
 from flask import Flask, request
 import json
+from createClass import class_blueprint
+from flask_cors import CORS
+# from ping import ping_blueprint
 
 app = Flask(__name__, static_folder="../build", static_url_path='/')
+CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
 	print('hello')
 	return app.send_static_file('index.html')
 
 @app.errorhandler(404)   
 def not_found(e):   
-  return app.send_static_file('index.html')
+	return "sdlk"
+  	# return app.send_static_file('index.html')
 
-from ping import ping_blueprint
-app.register_blueprint(ping_blueprint)
+# app.register_blueprint(ping_blueprint)
+app.register_blueprint(class_blueprint, url_prefix='/class')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000, debug=True)
