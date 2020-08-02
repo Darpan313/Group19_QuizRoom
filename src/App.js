31d51a3d5362103069880d96c66134520188940f
Darpan Patel <darpanpatel3131@gmail.com>
2020-08-02 09:31:14 -0300

    merged error solved

--- src/App.js
+++ src/App.js
@@ -22,17 +22,11 @@ import { UserContext } from "./context/user";
 import Footer from "./components/Footer";
 import CreateQuiz from "./components/CreateQuiz";
 import StudentDashboardNavigation from "./components/Student/StudentDashboardNavigation";
-<<<<<<< HEAD
+// import StudentDashboard from "./components/Student/StudentDashboard";
+import StudentClassroom from "./components/Student/StudentClassroom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import DeleteUser from "./components/DeleteUser";
-=======
-// import StudentDashboard from "./components/Student/StudentDashboard";
-import StudentClassroom from "./components/Student/StudentClassroom";
-import Login from './components/Login';
-import Register from './components/Register';
-import DeleteUser from './components/DeleteUser';
->>>>>>> d939ed24b9689ef3d533702d2e1d1f2513bc3ca3
 function App() {
   const { user } = React.useContext(UserContext);
   if (user.token && user.role == "Manager") {
@@ -77,7 +71,10 @@ function App() {
             <Route path="/startquiz" component={StartQuiz}></Route>
             <Route path="/faqs" component={Support}></Route>
             <Route path="/certificate" component={Certificate}></Route>
-            <Route path="/StudentClassroom" component={StudentClassroom}></Route>
+            <Route
+              path="/StudentClassroom"
+              component={StudentClassroom}
+            ></Route>
           </Switch>
         </div>
       </BrowserRouter>
